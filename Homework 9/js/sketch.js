e48aa77bcab72fd770b3beb9356749ed7e59f250
Michael Cassens <michaelcassens@gmail.com>
2020-10-11 09:29:32 -0600

    Uploading files.

---
+++ Homework 9/js/sketch.js
@@ -0,0 +1,13 @@
+function setup()
+{
+    createCanvas(500, 600);
+}
+
+function draw()
+{
+    background(120,45,78);
+    fill(255, 204, 0);
+    circle(250,100,175);
+    fill(10, 24, 120);
+    rect(200,185,100,150);
+}
